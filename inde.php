9113a4e4e78bef2c32e414bb12fed1f623e57d01
De-Chef (Recipe Sharing- Group H) <163147605+foodrecipegrouph@users.noreply.github.com>
2024-04-15 14:38:22 +0100

    inde.php

--- inde.php
+++ inde.php
@@ -76,7 +76,7 @@
                     <div class="row">
                         <div class="col-md-3">
                             <div class="full">
-                                <a class="logo" href="inde.php"><img src="images/logo.jpeg" height='70px' width ='70px' alt="logo" /></a>
+                                <a class="logo" href="inde.php"><img src="images/logo.jpg" height='70px' width ='70px' alt="logo" /></a>
                             </div>
                         </div>
                         <div class="col-md-9">
@@ -197,14 +197,14 @@
                 <div class="row">
                     <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
                         <div class="about_box">
-                            <h3>Best Food</h3>
+                            <h3>Be a good chef</h3>
                             <p> </p>
                             <a href="blog">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                         </div>
                     </div>
                     <div class="col-xl-5 col-lg-5 col-md-10 col-sm-12 about_img_boxpdnt">
                         <div class="about_img">
-                            <figure><img src="images/about-img.jpg" alt="#/"></figure>
+                            <figure><img src="images/G7.jpeg" alt="#/"></figure>
                         </div>
                     </div>
                 </div>
